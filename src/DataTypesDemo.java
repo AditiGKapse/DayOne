a6e6336afb3a727db05b6370e897361d2129de65
AditiGKapse <aditikapse92@gmail.com>
2024-07-12 20:26:07 +0530

    Add files via upload

---
+++ src/DataTypesDemo.java
@@ -0,0 +1,26 @@
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+
+		int value1 = 9 / 2; 
+		
+		System.out.println(value1);
+		float value2 = 101f /61f;
+		
+		System.out.println(value2);
+		
+		double value3 = 10d/6d;
+		
+		float value4 = 5/2f;
+		System.out.println(value4);
+		
+		
+		int radius=10;
+		double percentage =3.14 * radius  * radius;
+		
+		System.out.println("Percentage : "+ percentage);
+		
+		
+	}
+
+}
---
+++ src/IdentiferDemo.java
@@ -0,0 +1,16 @@
+public class IdentiferDemo {
+
+	public static void main(String[] args) {
+		int $num1= 20;
+		
+		System.out.println("Values of the number varible is :"+ $num1);
+		
+		String studentName="Shreya";
+		
+		System.out.println("value is "+ studentName);
+		
+		
+		
+	}
+
+}
